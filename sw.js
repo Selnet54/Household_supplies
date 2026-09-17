38247abc097262927a2277b893a85af0a30c5dc6
Selnet54 <selnet54@gmail.com>
2026-09-17 21:49:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,16 @@
+const CACHE_NAME = 'zalihe-v1';
+
 self.addEventListener('install', (e) => {
+  console.log('📦 SW install');
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  return self.clients.claim();
+  console.log('📦 SW activate');
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
+  // 🔥 OVO POKRIVA SVE PUTANJE
   e.respondWith(fetch(e.request));
 });
